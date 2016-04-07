cba9426ea9e5de16ba80be90462e10a8a6ea429b
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-04-06 23:37:46 -0500

    fixing build error

---
+++ CoolPlaces/Build/Intermediates/CoolPlaces.build/Debug-iphonesimulator/CoolPlaces.build/Objects-normal/x86_64/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rodrigo.s.mulkey/Desktop/Interview/CoolPlaces/CoolPlaces/CoolPlaces/AppDelegate.m \
+  /Users/rodrigo.s.mulkey/Desktop/Interview/CoolPlaces/CoolPlaces/CoolPlaces/AppDelegate.h
