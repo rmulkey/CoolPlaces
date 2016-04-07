b325252e3cced7bf70987c6db0cfcc0aa4a8fe03
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-04-06 22:36:32 -0500

    adding constants file with endpoints/paths

---
+++ CoolPlaces/CoolPlaces/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  CoolPlaces
+//
+//  Created by Mulkey, Rodrigo S. on 4/6/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define imagePath @"http://challenge.superfling.com/photos/"
+#define dataPath @"http://challenge.superfling.com"
+
+#endif /* Constants_h */
